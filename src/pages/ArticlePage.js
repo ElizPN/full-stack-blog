b0b8f3f9f0f0270c5f73ac2206f3d2bae7ce0204
ElizPN <pankevych.liza@gmail.com>
2023-08-24 21:21:36 +0200

    create main components

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,5 @@
+const ArticlePage = () => {
+  return <h1>This is the About Page</h1>;
+};
+
+export default ArticlePage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,5 @@
+const ArticlesListPage = () => {
+  return <h1>This is the ArticlePage Page</h1>;
+};
+
+export default ArticlesListPage;
