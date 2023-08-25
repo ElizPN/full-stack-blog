71d089c3bf5c8afd520a97c20c65544fa8d5ab0d
ElizPN <pankevych.liza@gmail.com>
2023-08-25 10:16:00 +0200

    import pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
 
 function App() {
   return (
     <div className="App">
-    <h1>My Blog</h1>
-    <div id='page-body'>
-      Welcome to my blog!
-    </div>
+      <h1>My Blog</h1>
+      <div id="page-body">
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
+      </div>
     </div>
   );
 }
