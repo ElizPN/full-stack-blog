21fa89105eb3f76377eccce8e331e15032a78d5a
ElizPN <pankevych.liza@gmail.com>
2023-08-25 14:46:15 +0200

    render articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,23 @@
+import { useParams } from "react-router";
+import articles from "./article-content";
+
 const ArticlePage = () => {
-  return <h1>This is the Article Page</h1>;
+  // articleId - the value of articleId will be what we type in browser line
+  const { articleId } = useParams();
+
+  // if value of articleId equel article.name
+  // articles - is array of article conetent
+  const article = articles.find(article => article.name === articleId);
+  console.log(article);
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map(paragraph => (
+        <p key={paragraph}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
