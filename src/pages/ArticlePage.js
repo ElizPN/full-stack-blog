3fd42afa00c6dbf9c85d2268f7303dd84138bda9
ElizPN <pankevych.liza@gmail.com>
2023-08-26 17:15:50 +0200

    refactor

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
   // articleId - the value of articleId will be what we type in browser line
@@ -9,6 +10,10 @@ const ArticlePage = () => {
   // articles - is array of article conetent
   const article = articles.find(article => article.name === articleId);
   console.log(article);
+
+  if (!article) {
+    return <NotFoundPage />;
+  }
 
   return (
     <>
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,5 +1,3 @@
-const NotFoundPage = () => {
-  return <h1>This is the NotFoundPage Page</h1>;
-};
+const NotFoundPage = () => <h1>The page not found 404</h1>;
 
 export default NotFoundPage;
