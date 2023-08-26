ff82d3c3dab0df8de3a028c4f88b31712cfb2cd2
ElizPN <pankevych.liza@gmail.com>
2023-08-26 16:32:42 +0200

    add article

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,5 +1,20 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticlesListPage = () => {
-  return <h1>This is the ArticlesList Page</h1>;
+  return (
+    <>
+      <h1>Articles List</h1>
+      {articles.map(article => (
+        <Link to={`./${article.name}`} key={article.name}>
+          <div>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}...</p>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default ArticlesListPage;
