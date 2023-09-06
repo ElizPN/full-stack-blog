3ec99198cc3b46cdb83a17601ae793f935fc9ea8
ElizPN <pankevych.liza@gmail.com>
2023-09-06 15:25:30 +0200

    get upvotes from db

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
 import { useParams } from "react-router";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   // articleId - the value of articleId will be what we type in browser line
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, []);
 
   // if value of articleId equel article.name
   // articles - is array of article conetent
@@ -18,6 +30,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map(paragraph => (
         <p key={paragraph}>{paragraph}</p>
       ))}
