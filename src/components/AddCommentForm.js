94e0a29df68e1ea815d10becc45e4be62de3bc89
ElizPN <pankevych.liza@gmail.com>
2023-09-09 20:11:47 +0200

    create add comment from

---
+++ src/components/AddCommentForm.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
+
+const AddCommentForm = ({ articleId }) => {
+  const [name, setName] = useState("");
+  const [commentText, setCommentText] = useState("");
+
+  const addComment = async () => {
+    const response = await axios.post(`/api/articles/${articleId}/comments`, {
+      postedBy: name,
+      text: commentText,
+    });
+
+    const updatedArticle = response.data;
+  };
+
+  return (
+    <div id="add-comment-form">
+      <h3>Add Comment</h3>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Comment:
+        <textarea
+          rows="4"
+          cols="50"
+          value={commentText}
+          onChange={e => setCommentText(e.target.value)}
+        />
+      </label>
+
+      <button>Add comment</button>
+    </div>
+  );
+};
+export default AddCommentForm;
