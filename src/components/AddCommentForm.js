01f423fcae1f748bcc7fd1eafb1d6d71fff69122
ElizPN <pankevych.liza@gmail.com>
2023-09-18 09:43:14 +0200

    use token in comment request

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 import { useState } from "react";
+import useUser from "../hooks/useUser";
 
 const AddCommentForm = ({ articleId, onArticleUpdated }) => {
   const [name, setName] = useState("");
   const [commentText, setCommentText] = useState("");
+  const { user } = useUser;
 
   const addComment = async () => {
-    const response = await axios.post(`/api/articles/${articleId}/comments`, {
-      postedBy: name,
-      text: commentText,
-    });
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+
+    const response = await axios.post(
+      `/api/articles/${articleId}/comments`,
+      {
+        postedBy: name,
+        text: commentText,
+      },
+      { headers },
+    );
 
     const updatedArticle = response.data;
     onArticleUpdated(updatedArticle);
